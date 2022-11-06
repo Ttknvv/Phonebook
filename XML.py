a1b5eb9aa9c62aabfea8eea5a4aab087ee7db762
Vova <vvvova24@gamil.com>
2022-11-06 16:27:25 +0600

    v2

--- XML.py
+++ XML.py
@@ -4,26 +4,30 @@ from sql import OutSet
 from sql import InsSet
 
 def ExpDataXml():
-    Phonebook = ET.Element("Phonebook")
+    root = ET.Element("Phonebook")
     data = OutSet()
     fileName = "Phonebook.xml"
     dirName = "export_files"
 
     for i in range(0, len(data)):
-        Phonebook.set(data[i][0], data[i][1])
+        pers = ET.SubElement(root, "pers")
+        pers.set("id", str(i))
+        name = ET.SubElement(pers, "name")
+        name.text = data[i][0]
+        number = ET.SubElement(pers, "number")
+        number.text = str(data[i][1])
 
-    xmlData = ET.tostring(Phonebook, encoding='unicode')
+    xmlData = ET.tostring(root, encoding='unicode')
     path = os.path.join(dirName, fileName)
     file = open(path, "w")
     file.write(xmlData)
+    file.close()
 
 def ImpDataXml():
     dirName = "import_files"
-    fileName = input("Введите название файла - ")
+    fileName = "Phonebook.xml" #input("Введите название файла - ")
     path = os.path.join(dirName, fileName)
-    allData = ET.parse(path)
-    data = allData.getroot()
-    print(data[0])
-
-ImpDataXml()
-
+    tree = ET.parse(path)
+    root = tree.getroot()
+    for i in range(0, len(root)):
+        InsSet(root[i][0].text, int(root[i][1].text))
--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 from sql import OutSet
 from sql import InsSet
+from sql import DelPers
+from sql import PrintPers
 from JSON import ExpDataJson
 from JSON import ImpDataJson
-from JSON import ExpDataXml
-from JSON import ImpDataXml
+from XML import ExpDataXml
+from XML import ImpDataXml
 
 print("Управление справочником, для выбора необходимой команды, введите число: ")
 while True:
     print("0 - Вывод данных таблицы\n"
           "1 - Добавление новой записи\n"
-          "2 - Экспорт данных\n"
-          "3 - Импорт данных\n"
-          "4 - Завершение")
+          "2 - Вывод информации по конкретной персоне\n"
+          "3 - Удаление информации по конкретной персоне\n"
+          "4 - Экспорт данных\n"
+          "5 - Импорт данных\n"
+          "6 - Завершение")
     usl = input("Ввод - ")
     if usl == "0":
         print(OutSet())
@@ -20,6 +24,13 @@ while True:
         number = int(input("Введите номер = "))
         InsSet(name, number)
     elif usl == "2":
+        print("Для вывода информации по человеку")
+        name = input("Введите имя человека - ")
+        print(PrintPers(name))
+    elif usl == "3":
+        name = input("Для удаление человека из справочника, введите имя - ")
+        DelPers(name)
+    elif usl == "4":
         print("Введите формат экспорта\n"
               "0 - JSON\n"
               "1 - XML")
@@ -28,7 +39,7 @@ while True:
             ExpDataJson()
         elif form == "1":
             ExpDataXml()
-    elif usl == "3":
+    elif usl == "5":
         print("Введите формат импорта\n"
               "0 - JSON\n"
               "1 - XML")
@@ -37,7 +48,7 @@ while True:
             ImpDataJson()
         elif form == "1":
             ImpDataXml()
-    elif usl == "4":
+    elif usl == "6":
         break
     else:
         print("Неправильный ввод, повтори попытку)")
